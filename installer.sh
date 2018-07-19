2b240f5e244cd35f5b03d97531522c9dab87890b
Marc Benedí <marcbenedi.sanmillan@gmail.com>
2018-07-19 12:28:17 +0200

    sync with master

--- installer.sh
+++ installer.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
 #Google test
+rm -rf googletest
+unzip googletest.zip
+cd googletest/googletest/make
+make
+mv gtest_main.a libgtest_main.a
+cd ..
+cd ..
+cd ..
 
 #CUDD
-tar zxvf cudd-3.0.0.zip
+rm -rf cudd-3.0.0
+unzip cudd-3.0.0.zip
+cd cudd-3.0.0
+./configure --enable-obj
+make
+make check
+cd ..
 
 #PBlib
-tar zxvf pblib.tgz
+rm -rf pblib
+unzip pblib.zip
+cd pblib
+cmake .
+make setup
+cd ..
 
-# cmake .
-# make setup
-#
-# make tfg
-# make tests
 
+#Requirements
+#picosat
+#cmake
+
+rm -rf test_build source_build
+mkdir test_build source_build
+
+make tfg
+make tests
